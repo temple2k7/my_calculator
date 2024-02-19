1e7638493af05f61f7fa389e54795c9008ca86f8
temple2k7 <100780812+temple2k7@users.noreply.github.com>
2024-02-19 17:23:36 +0100

    Created a calculator

---
+++ Calculator/index.js
@@ -0,0 +1,31 @@
+const question = document.getElementById("question")
+const display = document.getElementById("display")
+let working = ""
+let ans = 0
+
+function changeInput(displayed, value) {
+    working += value
+    question.innerHTML += displayed
+    display.innerHTML = displayed
+}
+function clearAll(){
+    question.innerHTML = ""
+    working = ""
+    display.innerHTML = ""
+}
+function backspace(){
+    display.innerHTML = ""
+    let spliced = question.innerHTML.slice(0, -1)
+    question.innerHTML = spliced
+
+    spliced = working.slice(0, -1)
+    working = spliced
+
+}
+
+function solution(){
+    console.log(working)
+    ans = eval(working).toString()
+    display.innerHTML = ans
+}
+
